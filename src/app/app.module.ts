f4c30e9826260a998320783d91287f864804497f
Kartheeswaran Manickam <kartheeswaran@Kartheeswarans-MacBook-Pro.local>
2022-10-10 13:12:18 +0530

    lottie home page anim image added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
+
+
+export function playerFactory(): any {  
+  return player;
+}
+
 
 @NgModule({
   declarations: [
@@ -10,7 +18,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LottieModule.forRoot({ player: playerFactory })
   ],
   providers: [],
   bootstrap: [AppComponent]
